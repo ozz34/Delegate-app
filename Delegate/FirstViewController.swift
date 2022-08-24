1e58b917dd72c62e3f8106cd5bfb9478dda2f692
IvanKhudyakov <halex7@yandex.ru>
2022-08-24 15:52:42 +0300

    доп задание по переходам

---
+++ Delegate/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Delegate
+//
+//  Created by brubru on 24.08.2022.
+//
+
+import UIKit
+
+protocol SecondViewControllerDelegate {
+    func getLabel(for text: String)
+}
+
+final class FirstViewController: UIViewController {
+    
+    @IBOutlet var smileLabel: UILabel!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let secondVC = segue.destination as? SecondViewController else { return }
+        secondVC.delegate = self
+        secondVC.textLabel = smileLabel.text
+    }
+}
+
+extension FirstViewController: SecondViewControllerDelegate {
+    
+    func getLabel(for text: String) {
+        smileLabel.text = text
+    }
+}
---
+++ Delegate/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  Delegate
+//
+//  Created by brubru on 24.08.2022.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var smileLabel: UILabel!
+    
+    var delegate: SecondViewControllerDelegate!
+    var textLabel: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField.delegate = self
+        smileLabel.text = textLabel
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        textField.endEditing(true)
+    }
+
+    @IBAction func goBackButtonPressed() {
+        delegate.getLabel(for: textField.text ?? "")
+        dismiss(animated: true)
+    }
+}
+    
+extension SecondViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text else { return }
+        smileLabel.text = text
+    }
+}
